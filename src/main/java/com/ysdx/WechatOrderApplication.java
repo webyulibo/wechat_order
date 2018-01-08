c357479cd1ee3e7884ecf188c3765133d018debe
yulibo <yulibo@daojia.com>
2018-01-08 20:48:30 +0800

    Signed-off-by: yulibo <yulibo@daojia.com>

---
+++ src/main/java/com/ysdx/WechatOrderApplication.java
@@ -0,0 +1,12 @@
+package com.ysdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatOrderApplication.class, args);
+	}
+}
